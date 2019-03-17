7216ec85ed8368d2d57494d92e5ab6af8191f449
Marczu <marczu09@gmail.com>
2019-03-17 11:27:33 +0100

    init commit

---
+++ src/main/java/com/mejner/myworklistservice/MyWorkListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mejner.myworklistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkListServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWorkListServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mejner/myworklistservice/MyWorkListServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mejner.myworklistservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyWorkListServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
